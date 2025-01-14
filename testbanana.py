67089494be9a2a50350fb15bcfa42865cd77fe65
Kullawipa Rattanapong <pittachiox123@gmail.com>
2025-01-14 15:53:07 +0700

    add testbanana

---
+++ testbanana.py
@@ -0,0 +1,113 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle
+from kivy.core.window import Window
+from kivy.clock import Clock
+from random import randint
+
+
+class Ball(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        with self.canvas:
+            # ใช้ภาพกล้วยแทนลูกบอล
+            self.ball = Rectangle(source='images/banana-removebg-preview.png', size=(80, 80), pos=(randint(0, Window.width - 80), Window.height - 80))
+        self.velocity_y = -200  # ความเร็วของลูกบอลในแนวตั้ง
+
+    def move(self, dt):
+        # การเคลื่อนไหวลูกบอล
+        bx, by = self.ball.pos
+        by += self.velocity_y * dt
+        self.ball.pos = (bx, by)
+
+    def reset(self):
+        # ตั้งค่าลูกบอลใหม่
+        self.ball.pos = (randint(0, Window.width - 50), Window.height - 50)
+
+
+class Paddle(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        with self.canvas:
+            self.paddle = Rectangle(size=(150, 20), pos=(Window.width / 2 - 75, 50))
+
+    def move(self, dt, pressed_keys):
+        # การเคลื่อนไหว paddle
+        cur_x, cur_y = self.paddle.pos
+        step = 300 * dt  # ความเร็วของ paddle
+        if 'a' in pressed_keys and cur_x > 0:
+            cur_x -= step
+        if 'd' in pressed_keys and cur_x < Window.width - self.paddle.size[0]:
+            cur_x += step
+        self.paddle.pos = (cur_x, cur_y)
+
+
+class BallCatchGame(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.score = 0
+
+        # สร้าง Paddle และ Ball
+        self.paddle = Paddle()
+        self.ball = Ball()
+        self.add_widget(self.paddle)
+        self.add_widget(self.ball)
+
+        # รับคีย์บอร์ด
+        self._keyboard = Window.request_keyboard(self._on_keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_key_down)
+        self._keyboard.bind(on_key_up=self._on_key_up)
+
+        self.pressed_keys = set()
+
+        # ตั้งเวลาให้เกมทำงาน
+        Clock.schedule_interval(self.update_game, 1 / 60)
+
+    def _on_keyboard_closed(self):
+        if self._keyboard:
+            self._keyboard.unbind(on_key_down=self._on_key_down)
+            self._keyboard.unbind(on_key_up=self._on_key_up)
+            self._keyboard = None
+
+    def _on_key_down(self, keyboard, keycode, text, modifiers):
+        self.pressed_keys.add(text)
+
+    def _on_key_up(self, keyboard, keycode):
+        text = keycode[1]
+        self.pressed_keys.discard(text)
+
+    def update_game(self, dt):
+        # อัปเดตการเคลื่อนไหวของ paddle และ ball
+        self.paddle.move(dt, self.pressed_keys)
+        self.ball.move(dt)
+
+        # ตรวจจับการชน
+        if self.check_collision():
+            self.score += 1
+            print(f"Score: {self.score}")
+            self.ball.reset()
+
+        # ตรวจสอบว่าลูกบอลตกถึงพื้น
+        if self.ball.ball.pos[1] < 0:
+            print("Game Over!")
+            self.ball.reset()
+
+    def check_collision(self):
+        # ตรวจสอบการชนระหว่าง paddle และ ball
+        px, py = self.paddle.paddle.pos
+        bx, by = self.ball.ball.pos
+        bw, bh = self.ball.ball.size
+        pw, ph = self.paddle.paddle.size
+
+        if (bx + bw > px) and (bx < px + pw) and (by < py + ph):
+            return True
+        return False
+
+
+class BallCatchApp(App):
+    def build(self):
+        return BallCatchGame()
+
+
+if __name__ == '__main__':
+    BallCatchApp().run()
