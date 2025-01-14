4b0557a051bee738685383892f9edd2fe46113bc
Kullawipa Rattanapong <pittachiox123@gmail.com>
2025-01-14 16:35:03 +0700

    chage paddle to stick

--- testbanana.py
+++ testbanana.py
@@ -6,30 +6,32 @@ from kivy.clock import Clock
 from random import randint
 
 
-class Ball(Widget):
+class banana(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         with self.canvas:
             # ใช้ภาพกล้วยแทนลูกบอล
-            self.ball = Rectangle(source='images/banana-removebg-preview.png', size=(80, 80), pos=(randint(0, Window.width - 80), Window.height - 80))
-        self.velocity_y = -200  # ความเร็วของลูกบอลในแนวตั้ง
+            self.banana = Rectangle(source='images/banana-removebg-preview.png', size=(80, 80), pos=(randint(0, Window.width - 80), Window.height - 80))
+        self.velocity_y = -400  # ความเร็วของลูกบอลในแนวตั้ง
 
     def move(self, dt):
         # การเคลื่อนไหวลูกบอล
-        bx, by = self.ball.pos
+        bx, by = self.banana.pos
         by += self.velocity_y * dt
-        self.ball.pos = (bx, by)
+        self.banana.pos = (bx, by)
 
     def reset(self):
         # ตั้งค่าลูกบอลใหม่
-        self.ball.pos = (randint(0, Window.width - 50), Window.height - 50)
+        self.banana.pos = (randint(0, Window.width - 50), Window.height - 50)
 
 
-class Paddle(Widget):
+class stick(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         with self.canvas:
-            self.paddle = Rectangle(size=(150, 20), pos=(Window.width / 2 - 75, 50))
+            # ใช้ภาพกิ่งไม้แทน paddle
+            self.paddle = Rectangle(source='images/stick-removebg-preview.png', size=(200, 150), pos=(Window.width / 2 - 100, 50))
+        self.velocity_y = -800
 
     def move(self, dt, pressed_keys):
         # การเคลื่อนไหว paddle
@@ -42,16 +44,17 @@ class Paddle(Widget):
         self.paddle.pos = (cur_x, cur_y)
 
 
-class BallCatchGame(Widget):
+
+class bananaCatchGame(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.score = 0
 
-        # สร้าง Paddle และ Ball
-        self.paddle = Paddle()
-        self.ball = Ball()
+        # สร้าง stick และ banana
+        self.paddle = stick()
+        self.banana = banana()
         self.add_widget(self.paddle)
-        self.add_widget(self.ball)
+        self.add_widget(self.banana)
 
         # รับคีย์บอร์ด
         self._keyboard = Window.request_keyboard(self._on_keyboard_closed, self)
@@ -77,26 +80,26 @@ class BallCatchGame(Widget):
         self.pressed_keys.discard(text)
 
     def update_game(self, dt):
-        # อัปเดตการเคลื่อนไหวของ paddle และ ball
+        # อัปเดตการเคลื่อนไหวของ paddle และ banana
         self.paddle.move(dt, self.pressed_keys)
-        self.ball.move(dt)
+        self.banana.move(dt)
 
         # ตรวจจับการชน
         if self.check_collision():
             self.score += 1
             print(f"Score: {self.score}")
-            self.ball.reset()
+            self.banana.reset()
 
         # ตรวจสอบว่าลูกบอลตกถึงพื้น
-        if self.ball.ball.pos[1] < 0:
+        if self.banana.banana.pos[1] < 0:
             print("Game Over!")
-            self.ball.reset()
+            self.banana.reset()
 
     def check_collision(self):
-        # ตรวจสอบการชนระหว่าง paddle และ ball
+        # ตรวจสอบการชนระหว่าง paddle และ banana
         px, py = self.paddle.paddle.pos
-        bx, by = self.ball.ball.pos
-        bw, bh = self.ball.ball.size
+        bx, by = self.banana.banana.pos
+        bw, bh = self.banana.banana.size
         pw, ph = self.paddle.paddle.size
 
         if (bx + bw > px) and (bx < px + pw) and (by < py + ph):
@@ -104,10 +107,10 @@ class BallCatchGame(Widget):
         return False
 
 
-class BallCatchApp(App):
+class bananaCatchApp(App):
     def build(self):
-        return BallCatchGame()
+        return bananaCatchGame()
 
 
 if __name__ == '__main__':
-    BallCatchApp().run()
+    bananaCatchApp().run()
